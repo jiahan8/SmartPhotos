0834a57f352d5f15da021200facf3de34104b136
Jia Han <jiahantan96@gmail.com>
2025-06-28 14:42:18 +0800

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.jiahan.smartcamera"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
