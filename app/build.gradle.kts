88c68029ccd530ec5ceacbfcb7004e12150bf791
Jia Han <jiahantan96@gmail.com>
2025-06-30 15:41:03 +0800

    Update version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 2
-        versionName = "1.1.0"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
